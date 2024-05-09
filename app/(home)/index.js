4952b5fe8b365c0595148ad0f9bd22a4daae322b
ZiedKnani <97735644+ZiedKnani@users.noreply.github.com>
2024-05-09 14:48:57 +0100

    Update index.js

--- app/(home)/index.js
+++ app/(home)/index.js
@@ -3,80 +3,129 @@ import { SafeAreaView, Text, View, StyleSheet, TextInput, TouchableOpacity, Scro
 import Ionicons from '@expo/vector-icons/Ionicons'
 import { events } from '../../constants/data';
 import { firestore_db } from '../../firebaseConfig';
-import { collection, getDoc, getDocs, limit, limitToLast, onSnapshot, query, startAfter} from 'firebase/firestore';
+import { collection, getDoc, getDocs, limit, limitToLast, onSnapshot, query, startAfter,where} from 'firebase/firestore';
 import Event from '../../components/event/Event.js';
 import { useRouter } from 'expo-router';
-
+import SelectDropdown from "react-native-select-dropdown"
+const categories = ["football", "chess", "party", "training", "yoga", "studies", "entertainment"];
 
 const Index = () => {
-    const [searchedText,setSearchedText]=useState('');
-    //get user logic ; 
-    const [events,setEvents]=useState(null)
+    const [selectedFilterCategory, setSelectedFilterCategory] = useState("");
+    const [selectedFilters, setSelectedFilters] = useState([]);
+    const [events, setEvents] = useState(null);
     const router=useRouter()
-    useEffect(()=>{
-        const eventsRef=collection(firestore_db,'events');
-        const subscriber=onSnapshot(eventsRef,{
-            next:(snapshot)=>{
-                const evts=[];
-                snapshot.forEach(doc => {
-                    evts.push({
-                        id:doc.id,
-                        ...doc.data(),
-                    })
-                });     
-                setEvents(evts);
-            }   
-        })
-    },[])
-    console.log(events)
-    return (
-        <SafeAreaView style={{flex:1,backgroundColor:"white",paddingHorizontal:5,marginTop:-35}}>
+    useEffect(() => {
+        const fetchEvents = async () => {
+            try {
+                const eventsRef = collection(firestore_db, 'events');
+                let eventsQuery = query(eventsRef);
 
+                if (selectedFilters.length > 0) {
+                    eventsQuery = query(eventsRef, where("category", "in", selectedFilters));
+                }
+
+                const unsubscribe = onSnapshot(eventsQuery, (snapshot) => {
+                    const updatedEvents = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+                    setEvents(updatedEvents);
+                });
+
+                return () => unsubscribe();
+            } catch (error) {
+                console.error("Error fetching events:", error);
+            }
+        };
+
+        fetchEvents();
+    }, [selectedFilters]);
+
+    const handleFilterDelete = (filter) => {
+        const updatedFilters = selectedFilters.filter(item => item !== filter);
+        setSelectedFilters(updatedFilters);
+    };
+
+    const renderSelectedFilters = () => {
+        return (
+            <View style={{ flexDirection: 'row', flexWrap: 'wrap', alignItems: 'center', marginTop: 10 }}>
+                {selectedFilters.map((filter, index) => (
+                    <View key={index} style={{ flexDirection: 'row', alignItems: 'center', marginRight: 10, marginBottom: 5 }}>
+                        <Text>{filter}</Text>
+                        <TouchableOpacity onPress={() => handleFilterDelete(filter)}>
+                            <Ionicons name="close-circle" size={20} color="red" style={{ marginLeft: 5 }} />
+                        </TouchableOpacity>
+                    </View>
+                ))}
+            </View>
+        );
+    };
+
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: "white", paddingHorizontal: 5, marginTop: -35 }}>
             {/* input field and add button container  */}
             <View style={styles.container} >
+                
                 <View style={styles.header}>
-                    <TextInput
-                        onChangeText={(text)=>setSearchedText(text)}
-                        value={searchedText}
-                        style={styles.TextInput}
-                        placeholder="Search all events"
-                     />
-                <View style={{justifyContent:"center",alignItems:"center"}}>
-                    <TouchableOpacity style={styles.button} onPress={()=>router.navigate('/createEvent')}>
-                                <Ionicons name='add-outline' color="pink" size={30} />
-                    </TouchableOpacity> 
-                </View>
-                </View>
-                <Text style={styles.headerText}>
+                    
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', width: '100%', paddingHorizontal: 10 }}>
+                      <Text style={styles.headerText}>
                         Discover
                 </Text>
+                <TouchableOpacity style={styles.button} onPress={()=>router.navigate('/createEvent')}>
+                                <Ionicons name='add-outline' color="pink" size={30} />
+                    </TouchableOpacity> 
+                    
+                </View>
+                </View>
+              
+            
+           
             </View>
-            {/* ********************************************* */}
-            {/* Events container  */}
-            <View style={{flex:1,marginHorizontal:10,justifyContent:"center",alignItems:"center"}} >
-                {events ? 
-                                <FlatList
-                                data={events}
-                                renderItem={
-                                    ({item})=>{
-                                        return <Event {...item}/> 
-                                    }
-                                }
-                                keyExtractor={item => item.id}
-                                showsVerticalScrollIndicator={false}
-                                style={{marginVertical:5}}
-                                
-                            />
-                            :<ActivityIndicator color="#000" size="large" /> }
-
-            </View>
-            {/* **************************************** */}
+            <View style={{ alignItems: 'center', marginTop: 10 }}>
+            <SelectDropdown style={{alignItems: 'center', paddingHorizontal: 10 }}
+                    data={categories}
+                    onSelect={(selectedItem, index) => {
+                        setSelectedFilterCategory(selectedItem);
+                        setSelectedFilters([...selectedFilters, selectedItem]);
+                    }}buttonTextAfterSelection={(selectedItem, index) => {
+                        // Conditionally render the text based on whether a filter is selected
+                        return selectedFilterCategory ? selectedFilterCategory : "Filter";
+                    }}
+                    rowTextForSelection={(item, index) => {
+                        return item;
+                    }}
+                    showsVerticalScrollIndicator={false}
+                    buttonStyle={{
+                        borderWidth: 2,
+                        borderRadius: 10,
+                        backgroundColor: "#fff",
+                    }}
+                    buttonTextStyle={{
+                        fontSize: 22,
+                        fontWeight: 500
+                    }}
+                    dropdownStyle={{
+                        borderRadius: 10,
+                    }}
+                    rowTextStyle={{
+                        color: "gray",
+                        fontWeight: 500
+                    }}
+                    defaultValueByIndex={0}
+                />
+            {renderSelectedFilters()}</View>
+            {events ? (
+                <FlatList
+                    data={events}
+                    renderItem={({ item }) => <Event {...item} />}
+                    keyExtractor={item => item.id}
+                    showsVerticalScrollIndicator={false}
+                    style={{ flex: 1 }}
+                />
+            ) : (
+                <ActivityIndicator color="#000" size="large" style={{ marginTop: 20 }} />
+            )}
         </SafeAreaView>
     );
 };
-
-
-
 
 export default Index;
 
